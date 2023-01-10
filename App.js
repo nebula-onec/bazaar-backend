3e245105f084e096642e209fed4b5eabe1738199
DipanshuJ <dipanshuj09@gmail.com>
2023-01-10 13:40:34 +0530

    Initial Setup

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-//console.log();
+const express = require('express');
+const app = express();
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({ path: "config/config.env" });
+}
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is running on {process.env. port}`);
+});
