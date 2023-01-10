97962f9b1d5a2196260d519ac4ce7d085f27f48b
DipanshuJ <103502287+DipanshuJ@users.noreply.github.com>
2023-01-10 13:46:27 +0530

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-// Config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({ path: "config/config.env" });
-}
-
-module.exports = app;
